0c327de6b621329a9d9acd56b317a499615cb828
sleepingfelisnigripes <sophielinfeicao@gmail.com>
2022-12-01 16:28:47 +1100

    react-learning-55

---
+++ 2_react_staging/react_staging/02_src_hello_react/App.js
@@ -0,0 +1,21 @@
+// 创建外壳组件APP(这块是用类式组件)
+// 能在这块引用Component是因为在React里面用‘分别暴露export’暴露了Component，而不是解构赋值
+import React, {Component} from 'react';
+import Hello from './components/Hello';
+import Welcome from './components/Welcome';
+
+//创建并暴露App组件
+export default class App extends Component{
+  render(){
+    return(
+      //因为APP.js是外壳，所以不要在div里面直接写东西，而是在div里面引用其他组件的标签
+      <div>
+        <Hello/>
+        <Welcome/>
+      </div>
+    )
+  }
+}
+
+// 暴露APP组件
+//export default App;
---
+++ 2_react_staging/react_staging/src/index.js
@@ -0,0 +1,10 @@
+// 引入react核心库
+import React from 'react';
+// 引入ReactDOM
+import ReactDOM from 'react-dom/client';
+// 引入APP组件
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+//ReactDOM.render(<App/>,document.getElementById('root'));
