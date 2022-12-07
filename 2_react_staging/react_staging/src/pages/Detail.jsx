395b1ecc2702189a3eb796c14e21a6bb30308ff1
sleepingfelisnigripes <sophielinfeicao@gmail.com>
2022-12-07 11:25:28 +1100

    react_learning_132-135

--- 2_react_staging/react_staging/src/pages/Detail.jsx
+++ 2_react_staging/react_staging/src/pages/Detail.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function Detail() {
-  //const {state:{id,title,content}} = useLocation()
+  const {state:{id,title,content}} = useLocation()
   console.log(useLocation());
    //search参数获取
   return (
     <div>
-        {/* <ul>
+        {<ul>
             <li>{id}</li>
             <li>{title}</li>
             <li>{content}</li>
-        </ul> */}
+        </ul>}
     </div>
   )
 }
